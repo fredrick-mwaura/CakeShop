941ea22b9d7c41593a62e8ffbb5f3163cdefb989
justoo67 <justinkirui3@gmail.com>
2024-10-18 14:39:26 +0300

    Header

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,37 +1,59 @@
-// import React from 'react'
-import '../stylesheets/footer.css'
+import "../stylesheets/footer.css";
 
 function Footer() {
-    const year = new Date().getFullYear;
+  const year = new Date().getFullYear(); // Fixed year issue
+
   return (
-    <div className='foot'>
-    <div className='footer'>
-
+    <div className="foot">
+      <div className="footer">
         <ul>
-            <h4 >Product</h4>
-            <li><a href="#">Product List</a></li>
-            <li><a href="#">Celebration Insights</a></li>
-            <li><a href="#">Flavours</a></li>
-            <li><a href="#">Cake Size Pricing</a></li>
-            <li><a href="#">Reviews</a></li>
+          <h4>Product</h4>
+          <li>
+            <a href="#">Product List</a>
+          </li>
+          <li>
+            <a href="#">Celebration Insights</a>
+          </li>
+          <li>
+            <a href="#">Flavours</a>
+          </li>
+          <li>
+            <a href="#">Cake Size Pricing</a>
+          </li>
+          <li>
+            <a href="#">Reviews</a>
+          </li>
         </ul>
         <ul>
-            <h4>Socials</h4>
-            <li><a href="instagram.com" target="_blank"><i className="fab fa-instagram-square"></i>Instagram</a></li>
-            <li><a href="facebook.com" target="_blank"><i className="fab fa-facebook-square"></i>Facebook</a></li>
-            <li><a href="https://www.x.com" target="_blank"><i className="fab fa-twitter-square"></i>Twitter</a></li>
+          <h4>Socials</h4>
+          <li>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram-square"></i> Instagram
+            </a>
+          </li>
+          <li>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook-square"></i> Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://www.x.com" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-twitter-square"></i> Twitter
+            </a>
+          </li>
         </ul>
-    <div>
-        <h4>Contact</h4>
-        <p>Phone: 123-456-7890</p>
-        <p>Email: baker@birthdaybaker.com</p>
-    </div>
-    </div>
+        <div>
+          <h4>Contact</h4>
+          <p>Phone: 123-456-7890</p>
+          <a href="mailto:example@gmail.com">example@gmail.com</a>
+        </div>
+      </div>
 
-    <div  className='footer-bottom'>
+      <div className="footer-bottom">
         <p>© {year} Cakes by Birthday Baker. All rights reserved.</p>
       </div>
     </div>
-  )
+  );
 }
+
 export default Footer;
