dd695d969bb0628ab11b698d2216ec381bc58488
fredrick-mwwaura <mwaurafredrick691@gmail.com>
2024-12-11 11:32:54 +0300

    provider

--- src/components/contexts/restrict.jsx
+++ src/components/contexts/restrict.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "./AuthContext.jsx";
 
 const Restricted = ({ children }) => {
   const { isAuthenticated } = useAuth();
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// // https://vite.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/', // Use '/' for static hosting or adjust as needed for deployment
   build: {
-    outDir: "dist", // You can change 'build' to any folder name
-    sourcemap: false,  // Disable source maps for production builds
-  },  
-  server: {
-    sourcemap: true, // Enable source maps during development
+    outDir: 'dist',
+    sourcemap: false, // Disable source maps for production for better security
   },
-  // server: {
-  //   proxy: {
-  //     '/cake-backend': {
-  //       target: 'http://localhost',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/cake-backend/, ''),
-  //     },
-  //   },
-  // },
-
+  server: {
+    proxy: {
+      '/cake-backend': {
+        target: 'http://localhost', // Backend API URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cake-backend/, ''),
+      },
+    },
+  },
 });
-
